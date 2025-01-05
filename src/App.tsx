be7fd8ca700335a6a43e93c79313a0074421cafd
Gregor Pawlak <greqorian@gmail.com>
2025-01-05 20:41:26 +0100

    feat: install open ai

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return <div className="container"></div>;
 }
 
 export default App;
